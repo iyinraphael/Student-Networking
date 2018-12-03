3d5b9cfcb301b5cca2421fa6bdb8cafbb3c5564c
iyinraphael <iyinraphael@gmail.com>
2018-12-03 10:01:48 -0800

    created model and network typr

---
+++ Student Networking/Model/Student.swift
@@ -0,0 +1,13 @@
+//
+//  Student.swift
+//  Student Networking
+//
+//  Created by Iyin Raphael on 12/3/18.
+//  Copyright © 2018 Iyin Raphael. All rights reserved.
+//
+
+import Foundation
+
+struct Student: Codable {
+    var name: String
+}
---
+++ Student Networking/Networking/NetworkClient.swift
@@ -0,0 +1,36 @@
+//
+//  NetworkClient.swift
+//  Student Networking
+//
+//  Created by Iyin Raphael on 12/3/18.
+//  Copyright © 2018 Iyin Raphael. All rights reserved.
+//
+
+import Foundation
+
+struct NetworkClient {
+    
+    static let url = URL(string: "https://studentdirectory.vapor.cloud/students")!
+    
+    func fetchRecipies(completion: @escaping ([Student]?, Error?) -> Void) {
+        URLSession.shared.dataTask(with: NetworkClient.url) { (data, _, error) in
+            if let error = error {
+                completion(nil, error)
+                return
+            }
+            guard let data = data else {
+                completion(nil, NSError())
+                return
+            }
+            do {
+                let recipes = try JSONDecoder().decode([Student].self, from: data)
+                completion(recipes, nil)
+            } catch {
+                completion(nil, error)
+                return
+            }
+        }
+        
+    }
+    
+}
