9ee372dd4d8fd4b1ea97ad961168e35ed76d93ee
iyinraphael <iyinraphael@gmail.com>
2018-12-03 10:52:11 -0800

    finished implementing methods in tableView

--- Student Networking/View Controller/MainViewController.swift
+++ Student Networking/View Controller/MainViewController.swift
@@ -13,21 +13,22 @@ class MainViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
     @IBAction func sort(_ sender: Any) {
     }
     
     @IBOutlet weak var sortSelector: UISegmentedControl!
     
-    /*
+    
     // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        if segue.identifier == "EmbedStudentsTableView" {
+            let studentTableVC = segue.destination as! StudentTableViewController
+            studentTableViewController = studentTableVC
+        }
     }
-    */
+    
+    private var studentTableViewController: StudentTableViewController!
 
 }
