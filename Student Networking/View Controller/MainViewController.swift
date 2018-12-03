4b17f0acb21541362c9cedfda5b8026ea16bd960
iyinraphael <iyinraphael@gmail.com>
2018-12-03 10:36:35 -0800

    storyboard layouts created and connected to class type

---
+++ Student Networking/View Controller/MainViewController.swift
@@ -0,0 +1,33 @@
+//
+//  MainViewController.swift
+//  Student Networking
+//
+//  Created by Iyin Raphael on 12/3/18.
+//  Copyright © 2018 Iyin Raphael. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    @IBAction func sort(_ sender: Any) {
+    }
+    
+    @IBOutlet weak var sortSelector: UISegmentedControl!
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
